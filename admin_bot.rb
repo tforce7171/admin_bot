b87b3fdb3a905fc662bef3202f99e9f43bcbe186
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2020-11-27 10:35:49 +0900

    optimized

--- admin_bot.rb
+++ admin_bot.rb
@@ -12,7 +12,9 @@ prefix:'/',
 application_id = ENV['APPLICATION_ID']
 clan_ids = ["1845","6800","29274","34796","16297"]
 access_token = ENV['ACCESS_TOKEN']
-channel_id_thirty = "451034405721473026"
+#channel_id_thirty = "451034405721473026"#本番
+channel_id_thirty = "549143999814959124"#テスト
+member_ids_out = ""
 
 bot.command :buku do |event|
 
@@ -34,19 +36,26 @@ bot.command :buku do |event|
 
     result["data"]["#{clan_id}"]["members_ids"].each do |member_id|
 
-      url = "https://api.wotblitz.asia/wotb/account/info/?application_id=#{application_id}&access_token=#{access_token}&account_id=#{member_id}&fields=nickname%2Clast_battle_time"
-      client = HTTPClient.new
-      response = client.get(url)
-      result = JSON.parse(response.body)
+      if date_range <= date_since_last_battle
+        member_ids_out = member_ids_out + "#{member_id}%2C"
+      end
 
-      nickname = result["data"]["#{member_id}"]["nickname"]
-      last_battle_time_unix = result["data"]["#{member_id}"]["last_battle_time"]
+    end
+
+    member_ids_out = member_ids_out.chomp('%2C')
+
+    url = "https://api.wotblitz.asia/wotb/account/info/?application_id=#{application_id}&access_token=#{access_token}&account_id=#{member_ids_out}&fields=nickname%2Clast_battle_time"
+    client = HTTPClient.new
+    response = client.get(url)
+    results = JSON.parse(response.body)
+
+    results["data"].each do |member_data|
+
+      nickname = member_data["nickname"]
+      last_battle_time_unix = member_data["last_battle_time"]
       last_battle_date = Time.at(last_battle_time_unix).to_date
       date_since_last_battle = (today - last_battle_date).to_i
-
-      if date_range <= date_since_last_battle
-        bot.send_message(channel_id_thirty, "#{nickname} : #{date_since_last_battle}日")
-      end
+      bot.send_message(channel_id_thirty, "#{nickname} : #{date_since_last_battle}日")
 
     end
 
