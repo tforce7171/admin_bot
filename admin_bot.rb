b49096c41ab9eb0e54a1eb87cf6a57bdfac8bda9
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2020-11-27 21:44:00 +0900

    Add files via upload

--- admin_bot.rb
+++ admin_bot.rb
@@ -70,9 +70,9 @@ bot.command :test do |event|
 
   bot.send_message(channel_id_thirty,"#{access_token_test}")
 
-  url = "https://api.worldoftanks.asia/wot/auth/prolongate/?application_id=#{application_id}&access_token=#{access_token_test}"
+  url = "https://api.worldoftanks.asia/wot/auth/prolongate/"
   client = HTTPClient.new
-  response = client.post(url)
+  response = client.post(url,{application_id: "#{application_id}",access_token: "#{access_token_test}"})
   result = JSON.parse(response.body)
 
   bot.send_message(channel_id_thirty,"#{result}")
