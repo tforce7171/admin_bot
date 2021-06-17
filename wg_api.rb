0a79029ffc594ca1d0010513584a66481dbeed04
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2021-06-17 12:04:31 +0900

    Update wg_api.rb

--- wg_api.rb
+++ wg_api.rb
@@ -28,6 +28,7 @@ class WGAPI
     )
     result = conn.exec("SELECT * FROM wg_access_token")
     access_token = result[0]["wg_access_token"]
+    p access_token
     # result.each do |tuple|
     #   access_token = tuple["wg_access_token"]
     #   p access_token
